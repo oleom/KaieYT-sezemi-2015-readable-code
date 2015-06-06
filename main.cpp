a36036d44416570d1f1005c5f44e11407e8d1d4b
Kento Masukawa <masukawa@west.sd.keio.ac.jp>
2015-06-06 14:39:26 +0900

    spec5: IDを振る

--- main.cpp
+++ main.cpp
@@ -16,17 +16,22 @@ using namespace std;
 
 void task1();
 void task3(const char*);
+void task4(const char*);
 
 int main(int argc, const char * argv[]) {
-    task1();
-    task3(argv[1]);
+//    task1();
+//    task3(argv[1]);
+    task4(argv[1]);
+
     return 0;
 }
+
 
 void task1(){
     //”オムライス”を出力
     cout << "オムライス" << endl;
 }
+
 
 void task3(const char* filename){
     string recipefilename = filename;
@@ -43,5 +48,26 @@ void task3(const char* filename){
     while(getline(ifs, recipestr)){
         istringstream stream(recipestr);
         cout << recipestr << endl;
+    }
+}
+
+
+void task4(const char* filename){
+    string recipefilename = filename;
+    int recipeid = 0;
+
+    //ファイルオープン
+    ifstream ifs(recipefilename);
+    if(!ifs){
+        cout << "error : file is not exist" << endl;
+        exit(1);
+    }
+    
+    //ファイルの記述を行ごとにコンソール出力
+    string recipestr;
+    while(getline(ifs, recipestr)){
+        istringstream stream(recipestr);
+        recipeid++;
+        cout << recipeid << ": " << recipestr << endl;
     }
 }
