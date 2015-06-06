5955f9895f2f0aeb4fa34655f06419f6a6ff5e2b
KaieYT <a1302035@gmail.com>
2015-06-06 10:18:59 +0900

    Readme

---
+++ Sezemi/Sezemi/README.md
@@ -0,0 +1,2 @@
+# KaieYT-sezemi-2015-readable-code
+リーダブルコード勉強会2015
---
+++ Sezemi/Sezemi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sezemi
+//
+//  Created by Yasuda on 2015/06/06.
+//  Copyright (c) 2015年 Yasuda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
