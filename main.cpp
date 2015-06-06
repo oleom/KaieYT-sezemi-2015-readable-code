13dfbe1dfdbd88928966b9045253b83db0d96bc9
Kento Masukawa <masukawa@west.sd.keio.ac.jp>
2015-06-06 14:46:43 +0900

    spec6: ID検索可能に

--- main.cpp
+++ main.cpp
@@ -17,12 +17,13 @@ using namespace std;
 void task1();
 void task3(const char*);
 void task4(const char*);
+void task5(const char*,const char*);
 
 int main(int argc, const char * argv[]) {
 //    task1();
 //    task3(argv[1]);
-    task4(argv[1]);
-
+//    task4(argv[1]);
+    task5(argv[1],argv[2]);
     return 0;
 }
 
@@ -69,5 +70,28 @@ void task4(const char* filename){
         istringstream stream(recipestr);
         recipeid++;
         cout << recipeid << ": " << recipestr << endl;
+    }
+}
+
+void task5(const char* filename,const char* id){
+    string recipefilename = filename;
+    int selected_id = atoi(id);
+    int recipeid = 0;
+
+    //ファイルオープン
+    ifstream ifs(recipefilename);
+    if(!ifs){
+        cout << "error : file is not exist" << endl;
+        exit(1);
+    }
+    
+    //ファイルの記述を行ごとにコンソール出力
+    string recipestr;
+    while(getline(ifs, recipestr)){
+        istringstream stream(recipestr);
+        recipeid++;
+        if(selected_id == recipeid){
+        cout << recipeid << ": " << recipestr << endl;
+        }
     }
 }
--- memo.md
+++ memo.md
@@ -9,11 +9,12 @@ commit
 commit
 
 仕様3を実装開始
-80%実装
+実装終了
 task3()のファイル読み込み処理で，スコープは狭いが変数名が簡単に理解できるよう工夫．
 string recipefilename = filename;
 ifstream ifs(recipefilename);
 string recipestr;
+int selected_id
 など．
 
 ####実際のコード
