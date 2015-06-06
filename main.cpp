b22a631ace79f6f1f9006d6af79c8109afef845d
KaieYT <a1302035@gmail.com>
2015-06-06 10:57:47 +0900

    task1

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Sezemi
+//
+//  Created by Yasuda on 2015/06/06.
+//  Copyright (c) 2015年 Yasuda. All rights reserved.
+//
+
+#include <iostream>
+void task1();
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    task1();
+    return 0;
+}
+
+void task1(){
+    std::cout << "オムライス" << std::endl;
+}
---
+++ memo.md
@@ -0,0 +1,4 @@
+# KaieYT-sezemi-2015-readable-code
+リーダブルコード勉強会2015
+//開発言語
+C++
