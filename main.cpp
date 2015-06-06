f6a74b1e1efe6f55f6ef64a14a9c5ea7818e3422
KaieYT <a1302035@gmail.com>
2015-06-06 11:37:32 +0900

    task3

--- main.cpp
+++ main.cpp
@@ -5,10 +5,14 @@
 //  Created by Yasuda on 2015/06/06.
 //  Copyright (c) 2015年 Yasuda. All rights reserved.
 //
-
+#include <stdio.h>
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <istream>
+
 void task1();
-void task2();
+int task3();
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -20,6 +24,22 @@ void task1(){
     std::cout << "オムライス" << std::endl;
 }
 
-void task2(){
+int task3(){
+    std::string filename;
+    std::stringstream recipestr;
     
+    filename = "recipe-data.txt";
+    
+    std::ifstream ifs(filename);
+    if(!ifs){
+        std::cout << "error : file is not exist";
+        return -1;
+    }
+    
+    while(getline(ifs, recipestr)){
+        std::istringstream stream(recipestr);
+        std::cout << recipestr << std::endl;
+    }
+    
+    return 1;
 }
